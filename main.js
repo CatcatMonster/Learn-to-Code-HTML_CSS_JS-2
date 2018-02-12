f2b5666391565fcb1f61368346ec2732446afe6d
Doubleshot1122 <skibo1122@yahoo.com>
2018-02-12 13:45:42 -0800

    updated comments

--- main.js
+++ main.js
@@ -1,10 +1,9 @@
-// prompts for user name, later used in congratulate alerts see edited alerts ${user} below
-// this is outside of the function so that it is only called when the page is loaded/refreshed
+// prompts for user names, later used in congratulate alerts see edited alerts ${player#} below
 var player1 = prompt("Please enter player 1's name");
 var player2 = prompt("Please enter player 2's name");
 
 function playGame() {
-  // prompts the user for a choice and stores it as playerOneChoice variable
+  // prompts the users for a choice and stores it as the player's choice variable
   var playerOneChoice = prompt("Player 1 please choose rock, paper, or scissors");
   var playerTwoChoice = prompt("Player 2 please choose rock, paper, or scissors");
 
@@ -13,7 +12,7 @@ function playGame() {
     window.alert(`Both picked ${playerOneChoice}, its a TIE game!`)
   }
 
-  // evaluation of all possible outcomes when user picks 'rock' and computer doesn't pick 'rock'
+  // evaluation of all possible outcomes when player1 picks 'rock' and player2 doesn't pick 'rock'
   else if (playerOneChoice === "rock") {
     if (playerTwoChoice === "paper") {
       window.alert(`${playerTwoChoice} vs ${playerOneChoice}, ${player2} WINS!!!`)
@@ -22,7 +21,7 @@ function playGame() {
     }
   }
 
-  // evaluation of all possible outcomes when user picks 'paper' and computer doesn't pick 'paper'
+  // evaluation of all possible outcomes when player1 picks 'paper' and player2 doesn't pick 'paper'
   else if (playerOneChoice === "paper") {
     if (playerTwoChoice === "scissors") {
       window.alert(`${playerTwoChoice} vs ${playerOneChoice}, ${player2} WINS!!!`)
@@ -31,12 +30,12 @@ function playGame() {
     }
   }
 
-  // evaluation of all possible outcomes when user picks 'scissors' and computer doesn't pick 'scissors'
+  // evaluation of all possible outcomes when player1 picks 'scissors' and player2 doesn't pick 'scissors'
   else if (playerOneChoice === "scissors") {
     if (playerTwoChoice === "rock") {
-      window.alert(`${playerTwoChoice} vs ${playerOneChoice}, Computer WINS!!!`)
+      window.alert(`${playerTwoChoice} vs ${playerOneChoice}, ${player2} WINS!!!`)
     } else {
-      window.alert(`${playerTwoChoice} vs ${playerOneChoice}, ${user} WINS!!!`)
+      window.alert(`${playerTwoChoice} vs ${playerOneChoice}, ${player1} WINS!!!`)
     }
   }
 }
