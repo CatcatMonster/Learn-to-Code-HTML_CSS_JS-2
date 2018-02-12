331c47f862cf50ae2c2de8e7b1b8f854bce29a18
Doubleshot1122 <skibo1122@yahoo.com>
2018-02-12 13:43:27 -0800

    added solution for this question

--- main.js
+++ main.js
@@ -1,52 +1,42 @@
 // prompts for user name, later used in congratulate alerts see edited alerts ${user} below
 // this is outside of the function so that it is only called when the page is loaded/refreshed
-var user = prompt("Please enter your name");
+var player1 = prompt("Please enter player 1's name");
+var player2 = prompt("Please enter player 2's name");
 
 function playGame() {
-  // prompts the user for a choice and stores it as userChoice variable
-  var userChoice = prompt("Please choose rock, paper, or scissors");
-
-  // created a random number between 0-1 and stores it as computerChoice variable
-  var computerChoice = Math.random();
-
-  // re-assigns the computerChoice variable to a string of "rock / paper/ scissors" based on random value
-  if (computerChoice <= 0.33) {
-    computerChoice = "rock";
-  } else if (computerChoice <= 0.66) {
-    computerChoice = "paper";
-  } else {
-    computerChoice = "scissors"
-  }
+  // prompts the user for a choice and stores it as playerOneChoice variable
+  var playerOneChoice = prompt("Player 1 please choose rock, paper, or scissors");
+  var playerTwoChoice = prompt("Player 2 please choose rock, paper, or scissors");
 
   // evaluation of tie results
-  if (userChoice === computerChoice) {
-    window.alert(`Both picked ${userChoice}, its a TIE game!`)
+  if (playerOneChoice === playerTwoChoice) {
+    window.alert(`Both picked ${playerOneChoice}, its a TIE game!`)
   }
 
   // evaluation of all possible outcomes when user picks 'rock' and computer doesn't pick 'rock'
-  else if (userChoice === "rock") {
-    if (computerChoice === "paper") {
-      window.alert(`${computerChoice} vs ${userChoice}, Computer WINS!!!`)
+  else if (playerOneChoice === "rock") {
+    if (playerTwoChoice === "paper") {
+      window.alert(`${playerTwoChoice} vs ${playerOneChoice}, ${player2} WINS!!!`)
     } else {
-      window.alert(`${computerChoice} vs ${userChoice}, ${user} WINS!!!`)
+      window.alert(`${playerTwoChoice} vs ${playerOneChoice}, ${player1} WINS!!!`)
     }
   }
 
   // evaluation of all possible outcomes when user picks 'paper' and computer doesn't pick 'paper'
-  else if (userChoice === "paper") {
-    if (computerChoice === "scissors") {
-      window.alert(`${computerChoice} vs ${userChoice}, Computer WINS!!!`)
+  else if (playerOneChoice === "paper") {
+    if (playerTwoChoice === "scissors") {
+      window.alert(`${playerTwoChoice} vs ${playerOneChoice}, ${player2} WINS!!!`)
     } else {
-      window.alert(`${computerChoice} vs ${userChoice}, ${user} WINS!!!`)
+      window.alert(`${playerTwoChoice} vs ${playerOneChoice}, ${player1} WINS!!!`)
     }
   }
 
   // evaluation of all possible outcomes when user picks 'scissors' and computer doesn't pick 'scissors'
-  else if (userChoice === "scissors") {
-    if (computerChoice === "rock") {
-      window.alert(`${computerChoice} vs ${userChoice}, Computer WINS!!!`)
+  else if (playerOneChoice === "scissors") {
+    if (playerTwoChoice === "rock") {
+      window.alert(`${playerTwoChoice} vs ${playerOneChoice}, Computer WINS!!!`)
     } else {
-      window.alert(`${computerChoice} vs ${userChoice}, ${user} WINS!!!`)
+      window.alert(`${playerTwoChoice} vs ${playerOneChoice}, ${user} WINS!!!`)
     }
   }
 }
